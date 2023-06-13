fa0fab87acb0fc247343a0c6438040a6030ef85b
Md Safiyat Reza <safiyat@voereir.com>
2023-06-13 09:47:10 +0530

    On 3.4.1.
    Set the cursor to block. More prominently visible.
    Set cursor blink to true.
    
    Updated default terminal colours.
    
    Set the bell sound URL.
    
    Set 100k scrollback. Still has trouble with ^L though.
    
    Set webGLRenderer to false. Using canvas based rendering now.
    Slower, but supports transparent backgrounds.
    
    webLinksActivationKey set to meta, to behave like iterm2.
    
    Added configuration for pane navigation. Has support for dimming inactive panes.
    
    Added default keymap config. Will update as per requirement.
    
    Signed-off-by: Md Safiyat Reza <safiyat@voereir.com>

--- hyper.js
+++ hyper.js
@@ -24,16 +24,17 @@ module.exports = {
         // terminal text color under BLOCK cursor
         cursorAccentColor: '#000',
         // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+        // cursorShape: 'UNDERLINE',
         cursorShape: 'BLOCK',
         // set to `true` (without backticks and without quotes) for blinking cursor
-        cursorBlink: false,
+        cursorBlink: true,
         // color of the text
-        foregroundColor: '#fff',
+        foregroundColor: '#000000',
         // terminal background color
         // opacity is only supported on macOS
-        backgroundColor: '#000',
+        backgroundColor: '#ffffdd',
         // terminal selection color
-        selectionColor: 'rgba(248,28,229,0.3)',
+        selectionColor: '#000000',
         // border color (window, tabs)
         borderColor: '#333',
         // custom CSS to embed in the main window
@@ -103,11 +104,13 @@ module.exports = {
         //  2. false: turns off the bell
         bell: 'SOUND',
         // An absolute file path to a sound file on the machine.
-        // bellSoundURL: '/path/to/sound/file',
+        bellSoundURL: '/Users/safiyat/GitHub/hyper-conf/Tink.mp3',
+        // The number of rows to be persisted in terminal buffer for scrolling. Default: 1000
+        scrollback: 99999,
         // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
         copyOnSelect: false,
         // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-        defaultSSHApp: true,
+        defaultSSHApp: false,
         // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
         // selection is present (`true` by default on Windows and disables the context menu feature)
         quickEdit: false,
@@ -117,10 +120,10 @@ module.exports = {
         macOptionSelectionMode: 'vertical',
         // Whether to use the WebGL renderer. Set it to false to use canvas-based
         // rendering (slower, but supports transparent backgrounds)
-        webGLRenderer: true,
+        webGLRenderer: false,
         // keypress required for weblink activation: [ctrl|alt|meta|shift]
         // todo: does not pick up config changes automatically, need to restart terminal :/
-        webLinksActivationKey: '',
+        webLinksActivationKey: 'meta',
         // if `false` (without backticks and without quotes), Hyper will use ligatures provided by some fonts
         disableLigatures: true,
         // set to true to disable auto updates
@@ -130,6 +133,42 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
+
+        // Plugin Configuration
+        // hyperBorder: {
+        //     borderWidth: 10
+        // }
+        paneNavigation: {
+          debug: false,
+          hotkeys: {
+            navigation: {
+              up: 'ctrl+alt+up',
+              down: 'ctrl+alt+down',
+              left: 'ctrl+alt+left',
+              right: 'ctrl+alt+right'
+            },
+            // completed with 1-9 digits
+            jump_prefix: 'ctrl+alt',
+            // Added to jump and navigation hotkeys for pane permutation
+            permutation_modifier: 'shift',
+            // maximize: 'meta+enter'
+            maximize: 'meta+shift+enter'
+          },
+          // Show pane number
+          showIndicators: true,
+          // Will be completed with pane number
+          indicatorPrefix: '^⌥',
+          // Added to indicator <div>
+          indicatorStyle: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            fontSize: '10px'
+          },
+          focusOnMouseHover: false,
+          // Set to 1 to disable inactive panes dimming
+          inactivePaneOpacity: 0.5
+        }
     },
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
@@ -137,14 +176,69 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: ['hyperborder', 'hypercwd', 'hyper-search', 'hyper-pane'],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
     localPlugins: [],
     keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
-    },
+      "window:devtools": "command+alt+i",
+      "window:reload": "command+shift+r",
+      "window:reloadFull": "command+shift+f5",
+      "window:preferences": "command+,",
+      "zoom:reset": "command+0",
+      "zoom:in": [
+        "command+plus",
+        "command+="
+      ],
+      "zoom:out": "command+-",
+      "window:new": "command+n",
+      "window:minimize": "command+m",
+      // "window:zoom": "ctrl+alt+command+m",
+      // Setting it to the default on iTerm 2.
+      "window:zoom": "command+shift+0",
+      // "window:toggleFullScreen": "command+ctrl+f",
+      // Setting it to the default on iTerm 2.
+      "window:toggleFullScreen": "command+enter",
+      "window:close": "command+shift+w",
+      "tab:new": "command+t",
+      "tab:next": [
+        "command+shift+]",
+        "command+shift+right",
+        "command+alt+right",
+        "ctrl+tab"
+      ],
+      "tab:prev": [
+        "command+shift+[",
+        "command+shift+left",
+        "command+alt+left",
+        "ctrl+shift+tab"
+      ],
+      "tab:jump:prefix": "command",
+      "pane:next": "command+]",
+      "pane:prev": "command+[",
+      "pane:splitRight": "command+d",
+      "pane:splitDown": "command+shift+d",
+      "pane:close": "command+w",
+      "editor:undo": "command+z",
+      "editor:redo": "command+y",
+      "editor:cut": "command+x",
+      "editor:copy": "command+c",
+      "editor:paste": "command+v",
+      "editor:selectAll": "command+a",
+      "editor:search": "command+f",
+      "editor:search-close": "esc",
+      "editor:movePreviousWord": "alt+left",
+      "editor:moveNextWord": "alt+right",
+      "editor:moveBeginningLine": "command+left",
+      "editor:moveEndLine": "command+right",
+      "editor:deletePreviousWord": "alt+backspace",
+      "editor:deleteNextWord": "alt+delete",
+      "editor:deleteBeginningLine": "command+backspace",
+      "editor:deleteEndLine": "command+delete",
+      "editor:clearBuffer": "command+k",
+      "editor:break": "ctrl+c",
+      "plugins:update": "command+shift+u"
+  },
 };
 //# sourceMappingURL=config-default.js.map
